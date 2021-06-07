8adf44b0ea5839e491b7f160be7681f96a8366af
dallin <dallin.briggs@nearearth.aero>
2021-06-07 15:22:33 -0400

    updated youcompleteme submodule and config.sh to use clangd instead of clang.

--- config.sh
+++ config.sh
@@ -3,4 +3,4 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
 vim +BundleInstall +qall
 
-cd $DIR/bundle/YouCompleteMe && python3 install.py --clang-completer
+cd $DIR/bundle/YouCompleteMe && python3 install.py --clangd-completer
