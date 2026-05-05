0944154c6aaaf1d88820601d687aa803cebb8f9b
Dallin Briggs <dallinbriggs@gmail.com>
2026-05-05 11:29:06 -0600

    Added install node js install steps. Not tested.

--- config.sh
+++ config.sh
@@ -2,3 +2,8 @@
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
 vim +PlugInstall +qall
+
+# Install node.js
+sudo apt-get install curl
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/master/install.sh | bash
+nvm install --lts
